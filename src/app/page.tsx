ddb22546befe24348fc1e64c14e34f1f59c6ec36
yairsabag <109817835+yairsabag@users.noreply.github.com>
2025-06-27 13:50:57 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -662,7 +662,7 @@ export default function Home() {
                 </div>
                 <div style={{ color: '#8B5E3C', marginBottom: '0.75rem', fontSize: '0.95rem', display: 'flex', alignItems: 'flex-start', gap: '8px' }}>
                   <span style={{ color: '#8B5E3C', fontSize: '1rem' }}>•</span>
-                  5 Image Analysis / Month
+                  Shoping List
                 </div>
                 <div style={{ color: '#8B5E3C', marginBottom: '0.75rem', fontSize: '0.95rem', display: 'flex', alignItems: 'flex-start', gap: '8px' }}>
                   <span style={{ color: '#8B5E3C', fontSize: '1rem' }}>•</span>
